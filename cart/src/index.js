f04cdc319eef6ba4e479855ac7d7193b41919c21
nadmez <nadmez@yahoo.com>
2025-09-26 12:53:20 +0300

    More realistic cart item count

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,6 +1,8 @@
 import faker from "faker";
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart.</div>`;
+const cartText = `<div>You have ${Math.floor(
+  faker.random.number() / 1000
+)} items in your cart.</div>`;
 
 document.querySelector("#cart-dev").innerHTML = cartText;
 console.log("Cart app is loaded!");
