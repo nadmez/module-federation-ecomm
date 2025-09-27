50fb4dcdbaa73b922307720391faa3638abd8b88
nadmez <nadmez@yahoo.com>
2025-09-27 10:02:11 +0300

    Faker module is share among modules1

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,8 +1,1 @@
-import faker from "faker";
-
-const cartText = `<div>You have ${Math.floor(
-  faker.random.number() / 1000
-)} items in your cart.</div>`;
-
-document.querySelector("#cart-dev").innerHTML = cartText;
-console.log("Cart app is loaded!");
+import("./bootstap");
